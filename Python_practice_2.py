b0be9e8dfe24354f1cd94c441b71f5952300bcf8
lydiadingess <lydiadingess@gmail.com>
2022-06-30 19:56:31 -0400

    Adding PyPoll.py file.

---
+++ Python_practice_2.py
@@ -0,0 +1,12 @@
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
+                {"county":"Denver", "registered_voters": 463353}, 
+                {"county":"Jefferson", "registered_voters": 432438}]
+for i in voting_data:
+    print(f'{i["county"]} has {i["registered_voters"]:,} registered voters')
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
