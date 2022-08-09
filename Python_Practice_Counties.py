c874ee8c3d013f2d1eab22a016b613d0d4309985
lydiadingess <lydiadingess@gmail.com>
2022-08-08 22:21:16 -0400

    adding the election audit code.

--- Python_Practice_Counties.py
+++ Python_Practice_Counties.py
@@ -1,3 +1,7 @@
-counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
 for county, voters in counties_dict.items():
     print(f"{county} county has {voters:,} registered voters.")
+
+voting_data = [``{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    print(f"{county_dict['county']} county has {county_dict['registered_voters']:,} registered voters.")
